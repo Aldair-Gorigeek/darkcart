417011aa4c9bf5ede226086dc1a053b008ee632c
Aldair Cabrera <aldair.cabrera@gorigeek.com>
2025-02-24 10:21:03 -0600

    Commit para validar que todo valla bien

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Header cartCount={cartItems.length} /> {/* Pasamos el contador al Header */}
+        <Header cartCount={cartItems.length} />
         <Routes>
           <Route path="/" element={<Home addToCart={addToCart} />} />
           <Route
